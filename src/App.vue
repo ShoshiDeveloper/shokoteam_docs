7490499e96de4dac4c6495e304b64cb2214498a9
shsohi-pc <lobachev-2019@mail.ru>
2024-05-16 23:11:14 +0500

    test

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@ import { RouterLink, RouterView } from 'vue-router'
   <!-- <RouterLink to="/blog/post?uid=ya7dy78ads">test</RouterLink> -->
   <div class="py-[64px] bg-[#18181B] min-h-screen text-white font-mplus px-[24px] xl:px-[256px]">
     <div class="min-h-full">
+        <RouterLink to="/forespolicy" :class="{}"></RouterLink>
       <RouterView />
     </div>
   </div>
